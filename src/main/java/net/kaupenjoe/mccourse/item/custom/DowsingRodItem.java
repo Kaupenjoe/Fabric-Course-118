67811404388a1ccf3d12c7f7d8fcf75183066341
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-12-05 16:53:52 +0100

    tooltip

--- src/main/java/net/kaupenjoe/mccourse/item/custom/DowsingRodItem.java
+++ src/main/java/net/kaupenjoe/mccourse/item/custom/DowsingRodItem.java
@@ -2,13 +2,21 @@ package net.kaupenjoe.mccourse.item.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class DowsingRodItem extends Item {
     public DowsingRodItem(Settings settings) {
@@ -41,6 +49,15 @@ public class DowsingRodItem extends Item {
                 (player) -> player.sendToolBreakStatus(player.getActiveHand()));
 
         return super.useOnBlock(context);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if(Screen.hasShiftDown()) {
+            tooltip.add(new TranslatableText("item.mccourse.dowsing_rode.tooltip.shift"));
+        } else {
+            tooltip.add(new TranslatableText("item.mccourse.dowsing_rode.tooltip"));
+        }
     }
 
     private void outputValuableCoordinates(BlockPos blockPos, PlayerEntity player, Block blockBelow) {
