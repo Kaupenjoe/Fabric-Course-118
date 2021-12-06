a7eca8f5c936f79feb5bc047d685455fe042040c
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-12-06 14:27:09 +0100

    tools

--- src/main/java/net/kaupenjoe/mccourse/item/ModItems.java
+++ src/main/java/net/kaupenjoe/mccourse/item/ModItems.java
@@ -3,7 +3,13 @@ package net.kaupenjoe.mccourse.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.kaupenjoe.mccourse.MCCourseMod;
 import net.kaupenjoe.mccourse.item.custom.DowsingRodItem;
+import net.kaupenjoe.mccourse.item.custom.ModAxeItem;
+import net.kaupenjoe.mccourse.item.custom.ModHoeItem;
+import net.kaupenjoe.mccourse.item.custom.ModPickaxeItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.SwordItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -26,6 +32,25 @@ public class ModItems {
 
     public static final Item COAL_SLIVER = registerItem("coal_sliver",
             new Item(new FabricItemSettings().group(ModItemGroups.COURSE)));
+
+
+    public static final Item ORICHALCUM_PICKAXE = registerItem("orichalcum_pickaxe",
+            new ModPickaxeItem(ModToolMaterial.ORICHALCUM, 1, 2f,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+    public static final Item ORICHALCUM_AXE = registerItem("orichalcum_axe",
+            new ModAxeItem(ModToolMaterial.ORICHALCUM, 3, 1f,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+    public static final Item ORICHALCUM_HOE = registerItem("orichalcum_hoe",
+            new ModHoeItem(ModToolMaterial.ORICHALCUM, 0, 0f,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+    public static final Item ORICHALCUM_SHOVEL = registerItem("orichalcum_shovel",
+            new ShovelItem(ModToolMaterial.ORICHALCUM, 1, 2f,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+    public static final Item ORICHALCUM_SWORD = registerItem("orichalcum_sword",
+            new SwordItem(ModToolMaterial.ORICHALCUM, 3, 3f,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+
+
 
 
     private static Item registerItem(String name, Item item) {
---
+++ src/main/java/net/kaupenjoe/mccourse/item/ModToolMaterial.java
@@ -0,0 +1,54 @@
+package net.kaupenjoe.mccourse.item;
+
+import net.fabricmc.yarn.constants.MiningLevels;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.util.Lazy;
+
+import java.util.function.Supplier;
+
+public enum ModToolMaterial implements ToolMaterial {
+    ORICHALCUM(MiningLevels.IRON, 450, 4.5F, 3.5F, 25, () ->
+            Ingredient.ofItems(ModItems.ORICHALCUM_INGOT));
+
+    private final int miningLevel;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Lazy<Ingredient> repairIngredient;
+
+    ModToolMaterial(int miningLevel, int itemDurability, float miningSpeed, float attackDamage,
+                            int enchantability, Supplier<Ingredient> repairIngredient) {
+        this.miningLevel = miningLevel;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = new Lazy(repairIngredient);
+    }
+
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    public int getMiningLevel() {
+        return this.miningLevel;
+    }
+
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return (Ingredient)this.repairIngredient.get();
+    }
+}
