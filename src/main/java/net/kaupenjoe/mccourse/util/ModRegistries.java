01222bd07441359cc2ee462a5f647985cac5655e
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-12-07 15:40:25 +0100

    commands

--- src/main/java/net/kaupenjoe/mccourse/util/ModRegistries.java
+++ src/main/java/net/kaupenjoe/mccourse/util/ModRegistries.java
@@ -1,7 +1,12 @@
 package net.kaupenjoe.mccourse.util;
 
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.kaupenjoe.mccourse.MCCourseMod;
+import net.kaupenjoe.mccourse.command.ReturnHomeCommand;
+import net.kaupenjoe.mccourse.command.SetHomeCommand;
+import net.kaupenjoe.mccourse.event.ModPlayerEventCopyFrom;
 import net.kaupenjoe.mccourse.item.ModItems;
 import net.minecraft.block.ComposterBlock;
 
@@ -9,6 +14,8 @@ public class ModRegistries {
     public static void registerModStuffs() {
         registerFuels();
         registerModComposterChances();
+        registerCommands();
+        registerEvents();
     }
 
     private static void registerFuels() {
@@ -22,5 +29,14 @@ public class ModRegistries {
     private static void registerModComposterChances() {
         ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.TURNIP_SEEDS, 0.35f);
         ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.TURNIP, 0.65f);
+    }
+
+    private static void registerCommands() {
+        CommandRegistrationCallback.EVENT.register(SetHomeCommand::register);
+        CommandRegistrationCallback.EVENT.register(ReturnHomeCommand::register);
+    }
+
+    private static void registerEvents() {
+        ServerPlayerEvents.COPY_FROM.register(new ModPlayerEventCopyFrom());
     }
 }
