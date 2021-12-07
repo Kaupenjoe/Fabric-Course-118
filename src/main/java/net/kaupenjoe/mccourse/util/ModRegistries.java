987006e62e26c2151493b0123bc2309cee397035
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-12-07 14:53:45 +0100

    composter

--- src/main/java/net/kaupenjoe/mccourse/util/ModRegistries.java
+++ src/main/java/net/kaupenjoe/mccourse/util/ModRegistries.java
@@ -3,10 +3,12 @@ package net.kaupenjoe.mccourse.util;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.kaupenjoe.mccourse.MCCourseMod;
 import net.kaupenjoe.mccourse.item.ModItems;
+import net.minecraft.block.ComposterBlock;
 
 public class ModRegistries {
     public static void registerModStuffs() {
         registerFuels();
+        registerModComposterChances();
     }
 
     private static void registerFuels() {
@@ -15,5 +17,10 @@ public class ModRegistries {
 
         // 400 / 20 = 20 Seconds
         registry.add(ModItems.COAL_SLIVER, 400);
+    }
+
+    private static void registerModComposterChances() {
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.TURNIP_SEEDS, 0.35f);
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.TURNIP, 0.65f);
     }
 }
