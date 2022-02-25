92110b891c4cdc0356e3e9c43127e9fc7eb82350
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-02-25 14:31:33 +0100

    entity spawn

---
+++ src/main/java/net/kaupenjoe/mccourse/world/gen/ModEntitySpawn.java
@@ -0,0 +1,27 @@
+package net.kaupenjoe.mccourse.world.gen;
+
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.mixin.object.builder.SpawnRestrictionAccessor;
+import net.kaupenjoe.mccourse.entity.ModEntities;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.biome.Biome;
+
+public class ModEntitySpawn {
+    public static void addEntitySpawn() {
+        BiomeModifications.addSpawn(BiomeSelectors.categories(Biome.Category.PLAINS), SpawnGroup.CREATURE,
+                ModEntities.RACCOON, 25, 2, 5);
+
+        BiomeModifications.addSpawn(BiomeSelectors.categories(Biome.Category.JUNGLE), SpawnGroup.CREATURE,
+                ModEntities.TIGER, 35, 2, 3);
+
+
+        SpawnRestrictionAccessor.callRegister(ModEntities.RACCOON, SpawnRestriction.Location.ON_GROUND,
+                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn);
+        SpawnRestrictionAccessor.callRegister(ModEntities.TIGER, SpawnRestriction.Location.ON_GROUND,
+                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn);
+    }
+}
--- src/main/java/net/kaupenjoe/mccourse/world/gen/ModWorldGen.java
+++ src/main/java/net/kaupenjoe/mccourse/world/gen/ModWorldGen.java
@@ -6,5 +6,7 @@ public class ModWorldGen {
 
         ModTreeGeneration.generateTrees();
         ModFlowerGeneration.generateFlowers();
+
+        ModEntitySpawn.addEntitySpawn();
     }
 }
