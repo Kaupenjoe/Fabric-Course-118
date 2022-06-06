11b088b61801865db9c570c815aa9d55fe28fd89
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-06-06 14:13:03 +0200

    redstone blocks

--- src/main/java/net/kaupenjoe/mccourse/block/custom/OrichalcumLampBlock.java
+++ src/main/java/net/kaupenjoe/mccourse/block/custom/OrichalcumLampBlock.java
@@ -3,6 +3,7 @@ package net.kaupenjoe.mccourse.block.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.ActionResult;
@@ -10,6 +11,8 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class OrichalcumLampBlock extends Block {
     public static final BooleanProperty CLICKED = BooleanProperty.of("clicked");
@@ -23,11 +26,35 @@ public class OrichalcumLampBlock extends Block {
     public ActionResult onUse(BlockState state, World world, BlockPos pos,
                               PlayerEntity player, Hand hand, BlockHitResult hit) {
         if(!world.isClient() && hand == Hand.MAIN_HAND) {
-            boolean clicked = state.get(CLICKED);
-            world.setBlockState(pos, state.with(CLICKED, !clicked),3);
+            System.out.println("Powered by " + world.getReceivedStrongRedstonePower(pos));
+            // boolean clicked = state.get(CLICKED);
+            // world.setBlockState(pos, state.with(CLICKED, !clicked),3);
         }
 
         return ActionResult.PASS;
+    }
+
+    @Override
+    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean notify) {
+        if (world.isClient) {
+            return;
+        }
+
+        boolean bl = state.get(CLICKED);
+        if (bl != world.isReceivingRedstonePower(pos)) {
+            if (bl) {
+                world.createAndScheduleBlockTick(pos, this, 4);
+            } else {
+                world.setBlockState(pos, state.cycle(CLICKED), Block.NOTIFY_LISTENERS);
+            }
+        }
+    }
+
+    @Override
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (state.get(CLICKED) && !world.isReceivingRedstonePower(pos)) {
+            world.setBlockState(pos, state.cycle(CLICKED), Block.NOTIFY_LISTENERS);
+        }
     }
 
     @Override
